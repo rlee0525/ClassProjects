5f80695415cb5c1f14fe71cf971c7c7f3c17fac0
Raymond Lee <raylee0525@gmail.com>
2016-12-13 18:08:06 -0800

    W5D2 Rails lite

--- W5/W5D2/skeleton/lib/controller_base.rb
+++ W5/W5D2/skeleton/lib/controller_base.rb
@@ -2,39 +2,60 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'active_support/inflector'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, {})
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "already built" if already_built_response?
+
+    @res.redirect(url, status = 302)
+    @already_built_response = true
+    self.session.store_session(@res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "already built" if already_built_response?
+
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @already_built_response = true
+    self.session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    content = File.read(path)
+    erb_content = ERB.new(content).result(binding)
+    render_content(erb_content, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
   end
 end
-
