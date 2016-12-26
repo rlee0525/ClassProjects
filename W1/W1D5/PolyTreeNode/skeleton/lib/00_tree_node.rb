cc74545eeacfda853ee887ec9c40903f7fdd6d89
Raymond Lee <raylee0525@gmail.com>
2016-12-26 11:10:42 -0800

    complete searchable module for dfs and bfs in polytreenode w1d5

--- W1/W1D5/PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ W1/W1D5/PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,34 @@
-class PolyTreeNode
+module Searchable
+  def dfs(target = nil, &prc)
+    raise "No proc or target" if [target, prc].none?
+    prc ||= Proc.new { |node| node.value == target }
 
+    return self if prc.call(self)
+
+    children.each do |child|
+      result = child.dfs(&prc)
+      return result unless result.nil?
+    end
+
+    nil
+  end
+
+  def bfs(target = nil, &prc)
+    raise "No proc or target" if [target, prc].none?
+    prc ||= Proc.new { |node| node.value == target }
+
+    nodes = [self]
+    until nodes.empty?
+      node = nodes.shift
+
+      return node if prc.call(node)
+      nodes.concat(node.children)
+    end
+
+    nil
+  end
+
+  def count
+    1 + children.map(&:count).inject(:+)
+  end
 end
