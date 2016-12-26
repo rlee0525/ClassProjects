5aec2c87f792421e9d8d7e885322b9d4ea377222
Raymond Lee <raylee0525@gmail.com>
2016-12-26 11:11:26 -0800

    complete polytreenode w1d5

--- W1/W1D5/PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ W1/W1D5/PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -32,3 +32,49 @@ module Searchable
     1 + children.map(&:count).inject(:+)
   end
 end
+
+class PolyTreeNode
+  include Searchable
+
+  attr_accessor :value
+  attr_reader :parent
+
+  def initialize(value = nil)
+    @value, @parent, @children = value, nil, []
+  end
+
+  def children
+    @children.dup
+  end
+
+  def parent=(parent)
+    return if self.parent == parent
+
+    if self.parent
+      self.parent._children.delete(self)
+    end
+
+    @parent = parent
+    self.parent._children << self unless self.parent.nil?
+
+    self
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    if child && !self.children.include?(child)
+      raise "Not a child"
+    end
+
+    child.parent = nil
+  end
+
+  protected
+
+  def _children
+    @children
+  end
+end
