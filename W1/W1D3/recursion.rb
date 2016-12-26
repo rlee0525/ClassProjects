71ab584ea48fcbb8279dfeb78e0d90e8118235f7
Raymond Lee <raylee0525@gmail.com>
2016-12-26 11:01:51 -0800

    complete recursion.rb exercise w1d3

--- W1/W1D3/recursion.rb
+++ W1/W1D3/recursion.rb
@@ -67,52 +67,32 @@ end
 def permutations(array)
   return [array] if array.length <= 1
 
-
-  # Similar to the subsets problem, we observe that to get the permutations
-  # of [1, 2, 3] we can look at the permutations of [1, 2] which are
-  # [1, 2] and [2, 1] and add the last element to every possible index getting
-  # [3, 1, 2], [1, 3, 2], [1, 2, 3], [3, 2, 1], [2, 3, 1]
-
-  # pop off the last element
   first = array.shift
-  # make the recursive call
   perms = permutations(array)
-  # we will need an array to store all our different permutations
   total_permutations = []
 
-
-  # Now we iterate over the result of our recusive call say [[1, 2], [2, 1]]
-  # and for each permutation add first into every index. This new subarray
-  # gets added to total_permutations.
   perms.each do |perm|
     (0..perm.length).each do |i|
-      total_permutations << perm[0 ... i] + [first] + perm[i .. -1]
+      total_permutations << perm[0...i] + [first] + perm[i..-1]
     end
   end
+
   total_permutations
 end
 
 def bsearch(nums, target)
-  # nil if not found; can't find anything in an empty array
   return nil if nums.empty?
 
   probe_index = nums.length / 2
   case target <=> nums[probe_index]
   when -1
-    # search in left
     bsearch(nums.take(probe_index), target)
   when 0
-    probe_index # found it!
+    probe_index
   when 1
-    # search in the right; don't forget that the right subarray starts
-    # at `probe_index + 1`, so we need to offset by that amount.
     sub_answer = bsearch(nums.drop(probe_index + 1), target)
     (sub_answer.nil?) ? nil : (probe_index + 1) + sub_answer
   end
-
-  # Note that the array size is always decreasing through each
-  # recursive call, so we'll either find the item, or eventually end
-  # up with an empty array.
 end
 
 class Array
@@ -139,46 +119,21 @@ class Array
 end
 
 def make_change(target, coins = [25, 10, 5, 1])
-  # Don't need any coins to make 0 cents change
   return [] if target == 0
-  # Can't make change if all the coins are too big. This is in case
-  # the coins are so weird that there isn't a 1 cent piece.
   return nil if coins.none? { |coin| coin <= target }
 
-  # Optimization: make sure coins are always sorted descending in
-  # size. We'll see why later.
   coins = coins.sort.reverse
 
   best_change = nil
   coins.each_with_index do |coin, index|
-    # can't use this coin, it's too big
     next if coin > target
 
-    # use this coin
     remainder = target - coin
-
-    # Find the best way to make change with the remainder (recursive
-    # call). Why `coins.drop(index)`? This is an optimization. Because
-    # we want to avoid double counting; imagine two ways to make
-    # change for 6 cents:
-    #   (1) first use a nickel, then a penny
-    #   (2) first use a penny, then a nickel
-    # To avoid double counting, we should require that we use *larger
-    # coins first*. This is what `coins.drop(index)` enforces; if we
-    # use a smaller coin, we can never go back to using larger coins
-    # later.
     best_remainder = make_change(remainder, coins.drop(index))
-
-    # We may not be able to make the remaining amount of change (e.g.,
-    # if coins doesn't have a 1cent piece), in which case we shouldn't
-    # use this coin.
     next if best_remainder.nil?
 
-    # Otherwise, the best way to make the change **using this coin**,
-    # is the best way to make the remainder, plus this one coin.
     this_change = [coin] + best_remainder
 
-    # Is this better than anything we've seen so far?
     if (best_change.nil? || (this_change.count < best_change.count))
       best_change = this_change
     end
