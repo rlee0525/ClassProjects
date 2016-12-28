da610005e9cdd2c425a3a1a74a32524aaca88914
Raymond Lee <raylee0525@gmail.com>
2016-12-28 15:47:47 -0800

    Init ajaxtwitter w6d3

--- W6/W6D3/skeleton/frontend/twitter.js
+++
@@ -1,9 +0,0 @@
-const FollowToggle = require('./follow_toggle.js');
-
-$(() => {
-  const $buttons = $('.follow-toggle');
-
-  $buttons.each( function(_, button) {
-    new FollowToggle(button);
-  });
-});
