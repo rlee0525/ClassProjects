fbc39a8550c6e8a49d235658e45025080e5d1732
Raymond Lee <raylee0525@gmail.com>
2016-12-30 14:34:18 -0800

    update webpack config w7d0

--- W7/W7D0/Synthesizer/webpack.config.js
+++ W7/W7D0/Synthesizer/webpack.config.js
@@ -1,17 +1,14 @@
-const path = require("path");
-
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/synthesizer.jsx",
+  entry: 'path/to/entry.jsx',
   output: {
-    path: path.join(__dirname),
-    filename: "bundle.js"
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
@@ -19,8 +16,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".js", ".jsx" ]
+    extensions: ['', '.js', '.jsx' ]
   }
 };
