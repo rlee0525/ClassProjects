0565d06ee307b19a8dfb8519f1e7764e7c40478b
Raymond Lee <raylee0525@gmail.com>
2016-12-30 15:03:11 -0800

    complete phas1 notes synthesizer w7d0

--- W7/W7D0/Synthesizer/frontend/synthesizer.jsx
+++ W7/W7D0/Synthesizer/frontend/synthesizer.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Note from './util/note';
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<h1>Synthesizer</h1>, root);
 });
