2a18ed506d8d166a4f4d79841649c315277f034d
Raymond Lee <raylee0525@gmail.com>
2017-01-01 17:21:26 -0800

    complete phase 2 todos redux structure

--- W7/W7D1/Todos/frontend/todo_redux.jsx
+++ W7/W7D1/Todos/frontend/todo_redux.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById("content");
   ReactDOM.render(<h1>Todos App</h1>, root);
+  window.store = store;
 });
