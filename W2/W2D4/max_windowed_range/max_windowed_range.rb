7bac749a0261f5f93583d39484b85a430ab4cc47
Raymond Lee <raylee0525@gmail.com>
2017-01-02 21:42:39 -0800

    implemented max_windowed_range using min_max_stack_queue

---
+++ W2/W2D4/max_windowed_range/max_windowed_range.rb
@@ -0,0 +1,39 @@
+require_relative "min_max_stack_queue"
+
+def max_windowed_range(array, window_size)
+  num_windows = array.length - window_size + 1
+  best_range = nil
+
+  num_windows.times do |i|
+    window = array.slice(i, window_size)
+    current_range = window.max - window.min
+
+    best_range = current_range if !best_range || current_range > best_range
+  end
+
+  best_range
+end
+
+def max_windowed_range(array, window_size)
+  queue = MinMaxStackQueue.new
+  best_range = nil
+
+  array.each_with_index do |el, i|
+    queue.enqueue(el)
+    queue.dequeue if queue.size > window_size
+
+    if queue.size == window_size
+      current_range = queue.max - queue.min
+      best_range = current_range if !best_range || current_range > best_range
+    end
+  end
+
+  best_range
+end
+
+if __FILE__ == $PROGRAM_NAME
+  p max_windowed_range([1, 0, 2, 5, 4, 8], 2) == 4
+  p max_windowed_range([1, 0, 2, 5, 4, 8], 3) == 5
+  p max_windowed_range([1, 0, 2, 5, 4, 8], 4) == 6
+  p max_windowed_range([1, 3, 2, 5, 4, 8], 5) == 6
+end
