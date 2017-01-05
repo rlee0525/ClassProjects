d91cfad9ff95b1c9d1c7db45c283cff3cedfc58e
Raymond Lee <raylee0525@gmail.com>
2017-01-05 10:44:06 -0800

    complete phase 2 pokemon redux cycle

--- W7/W7D3/Pokedex/frontend/pokedex.jsx
+++ W7/W7D3/Pokedex/frontend/pokedex.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { requestAllPokemon } from './actions/pokemon_actions';
+import { fetchAllPokemon } from './util/api_util';
+
+import { selectAllPokemon } from './reducers/selectors';
+
+import configureStore from './store/store';
+
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Pokemon</h1>, root);
+
+  window.requestAllPokemon = requestAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.store = store;
+
+  window.selectAllPokemon = selectAllPokemon;
 });
