6f4ac1c4a822b056fec8e7a509896c632a794315
Raymond Lee <raylee0525@gmail.com>
2017-01-06 14:50:56 -0800

    update react router phase 2 benchbnb

--- W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
+++ W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// TODO: testing
 import configureStore from './store/store';
+import Root from './components/root';
+
+// TODO: testing
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 
   // TODO: testing
   window.store = store;
