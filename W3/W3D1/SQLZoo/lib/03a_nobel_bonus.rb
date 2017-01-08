c6ccba4bb4fbfa065003614f52e5486020c1afba
Raymond Lee <raylee0525@gmail.com>
2017-01-07 23:47:29 -0800

    complete 03a_nobel_bonus

--- W3/W3D1/SQLZoo/lib/03a_nobel_bonus.rb
+++ W3/W3D1/SQLZoo/lib/03a_nobel_bonus.rb
@@ -8,11 +8,20 @@
 
 require_relative './sqlzoo.rb'
 
-# BONUS PROBLEM: requires sub-queries or joins. Attempt this after completing
-# sections 04 and 07.
-
 def physics_no_chemistry
-  # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT
+      yr
+    FROM
+      nobels
+    WHERE
+      (subject = 'Physics' AND yr NOT IN (
+        SELECT
+          yr
+        FROM
+          nobels
+        WHERE
+          subject = 'Chemistry'
+      ))
   SQL
 end
