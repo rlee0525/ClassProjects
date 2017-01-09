b1d4afa2057d576ad6dab2b473fc1d193538af33
Raymond Lee <raylee0525@gmail.com>
2017-01-09 00:24:16 -0800

    implement session api util

--- W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
+++ W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TODO:
+import { signup, login, logout } from './util/session_api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB!</h1>, root);
+
+// TODO: 
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 });
