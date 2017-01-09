cfa0cea60c24dcf7efb6a3c631f96e08eab230e2
Raymond Lee <raylee0525@gmail.com>
2017-01-09 01:49:34 -0800

    complete front end authentication phase 1

--- W7/W7D5/BenchBnB/app/models/user.rb
+++ W7/W7D5/BenchBnB/app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_by_credentials(username, password)
-    user = User.find_by_username(username)
+    user = User.find_by(username: username)
     user && user.is_password?(password) ? user : nil
   end
 
--- W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
+++ W7/W7D5/BenchBnB/frontend/bench_bnb.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 
 // TODO:
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
+import { receiveCurrentUser } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB!</h1>, root);
 
-// TODO: 
+// TODO:
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+  window.store = store;
+  window.receiveCurrentUser = receiveCurrentUser;
 });
