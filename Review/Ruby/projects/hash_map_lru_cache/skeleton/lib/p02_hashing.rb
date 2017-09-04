3d3f7650afb313ee7825415bdfdb78a0f1eb423a
Raymond Lee <raylee0525@gmail.com>
2017-09-04 12:57:16 -0700

    hashmap_p02

--- Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p02_hashing.rb
+++ Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    res = 0
+
+    self.each_with_index do |val, idx|
+      res ^= val.hash + idx.hash
+    end
+
+    res
   end
 end
 
 class String
   def hash
+    res = 0
+
+    self.chars.each_with_index do |char, idx|
+      res ^= char.ord.hash + idx.hash
+    end
+
+    res
   end
 end
 
@@ -16,6 +30,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by(&:hash).hash
   end
 end
