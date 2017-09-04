ecfe41db572b555bde7aed3c04cda0914bad1d09
Raymond Lee <raylee0525@gmail.com>
2017-09-04 13:38:53 -0700

    hashmap_p05

--- Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p05_hash_map.rb
+++ Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p05_hash_map.rb
@@ -1,5 +1,6 @@
 require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
+include Enumerable
 
 class HashMap
   attr_reader :count
@@ -10,18 +11,44 @@ class HashMap
   end
 
   def include?(key)
+    each do |k, v|
+      return true if k == key
+    end
+
+    false
   end
 
   def set(key, val)
+    resize! if @count == num_buckets
+    if bucket(key).include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    each do |k, v|
+      return v if k == key
+    end
+
+    nil
   end
 
   def delete(key)
+    if bucket(key).include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |list|
+        yield [list.key, list.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +69,17 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |list|
+        set(list.key, list.val)
+      end
+    end
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
