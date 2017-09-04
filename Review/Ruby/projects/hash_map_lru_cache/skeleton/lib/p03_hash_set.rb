93703afc0713a6015a9edf97fa1c6bcf163b52f3
Raymond Lee <raylee0525@gmail.com>
2017-09-04 13:03:15 -0700

    hashmap_p03

--- Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
+++ Review/Ruby/projects/hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
@@ -9,18 +9,27 @@ class HashSet
   end
 
   def insert(key)
+    return false if self[key.hash].include?(key)
+    resize! if @count == num_buckets
+    self[key.hash] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self[key.hash].include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets] << num
   end
 
   def num_buckets
@@ -28,5 +37,12 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self[el] << el
+      end
+    end
   end
 end
