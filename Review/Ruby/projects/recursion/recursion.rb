3e477c225082ed500ae1f57c1734eff03367e22d
Raymond Lee <raylee0525@gmail.com>
2017-09-18 15:51:06 -0700

    rec - warmup

--- Review/Ruby/projects/recursion/recursion.rb
+++ Review/Ruby/projects/recursion/recursion.rb
@@ -2,3 +2,44 @@ def range(s, e)
   return [] if e <= s
   range(s, e - 1) << e - 1
 end
+
+p range(3, 10)
+
+def rec_sum(arr)
+  return 0 if arr.empty?
+  arr[0] + rec_sum(arr[1..-1])
+end
+
+p rec_sum([3, 4, 5, 10])
+
+def iter_sum(arr)
+  sum = 0
+  arr.each do |el|
+    sum += el
+  end
+
+  sum
+end
+
+p iter_sum([3, 4, 5, 10])
+
+def exp(b, n)
+  return 1 if n == 0
+  b * exp(b, n - 1)
+end
+
+p exp(2, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
