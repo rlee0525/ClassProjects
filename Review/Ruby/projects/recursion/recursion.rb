24b35f9cdf894f1e9d6dedee6522d59bca24aced
Raymond Lee <raylee0525@gmail.com>
2017-09-18 16:05:40 -0700

    rec - exp

--- Review/Ruby/projects/recursion/recursion.rb
+++ Review/Ruby/projects/recursion/recursion.rb
@@ -3,14 +3,14 @@ def range(s, e)
   range(s, e - 1) << e - 1
 end
 
-p range(3, 10)
+p range(3, 10) == [3, 4, 5, 6, 7, 8, 9]
 
 def rec_sum(arr)
   return 0 if arr.empty?
   arr[0] + rec_sum(arr[1..-1])
 end
 
-p rec_sum([3, 4, 5, 10])
+p rec_sum([3, 4, 5, 10]) == 22
 
 def iter_sum(arr)
   sum = 0
@@ -21,17 +21,21 @@ def iter_sum(arr)
   sum
 end
 
-p iter_sum([3, 4, 5, 10])
+p iter_sum([3, 4, 5, 10]) == 22
 
 def exp(b, n)
   return 1 if n == 0
   b * exp(b, n - 1)
 end
 
-p exp(2, 5)
+p exp(2, 5) == 32
 
+def exp2(b, n)
+  return 1 if n == 0
+  n.even? ? exp2(b, n / 2) ** 2 : b * (exp2(b, (n - 1) / 2) ** 2)
+end
 
-
+p exp2(2, 5) == 32
 
 
 
