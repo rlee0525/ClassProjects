4dc35565afefe4bdf1c6d7e371e96ac6356e0fe0
Raymond Lee <raylee0525@gmail.com>
2017-10-17 15:10:48 -0700

    lca - depth

--- Graduated/W1/lca.js
+++ Graduated/W1/lca.js
@@ -13,7 +13,7 @@
 
 // With parent pointer
 // Time: O(h) Space: O(h) - Brute Force
-const LCA = (node1, node2) => {
+const LCA1 = (node1, node2) => {
   let seen = {};
 
   while (node1) {
@@ -30,3 +30,49 @@ const LCA = (node1, node2) => {
 };
 
 // Time: O(h) Space: O(1) - Depth Method
+const LCA2 = (node1, node2) => {
+  let depth1 = getDepth(node1);
+  let depth2 = getDepth(node2);
+
+  if (depth1 < depth2) {
+    let temp = node1;
+    node1 = node2;
+    node2 = temp;
+  }
+
+  let depthDifference = Math.abs(depth1 - depth2);
+  while (depthDifference !== 0) {
+    node1 = node1.parent;
+    depthDifference--;
+  }
+
+  while (node1 !== node2) {
+    node1 = node1.parent;
+    node2 = node2.parent;
+  }
+
+  return node1;
+};
+
+const getDepth = node => {
+  let depth = 0;
+
+  while (node) {
+    depth++;
+    node = node.parent;
+  }
+
+  return depth;
+};
+
+// Given Root & No parent
+
+
+
+
+
+
+
+
+
+
