602a28e3666a301f2931b4a09e2bdc03c117b39a
Raymond Lee <raylee0525@gmail.com>
2017-10-16 17:22:44 -0700

    maxmin

--- Graduated/W1/bst.js
+++ Graduated/W1/bst.js
@@ -56,6 +56,34 @@ class BinarySearchTree {
 
     return node;
   }
+
+  min(node = this.root) {
+    if (node.left) {
+      return this.min(node.left);
+    } else {
+      return node;
+    }
+  }
+
+  max(node = this.root) {
+    if (node.right) {
+      return this.max(node.right);
+    } else {
+      return node;
+    }
+  }
 }
+
+let bst = new BinarySearchTree();
+bst.insert(5);
+bst.insert(3);
+bst.insert(6);
+bst.insert(2);
+bst.insert(4);
+bst.insert(1);
+
+console.log(bst.root);
+console.log(bst.min());
+console.log(bst.max());
 
 module.exports = BinarySearchTree;
--- Graduated/W1/lca.js
+++ Graduated/W1/lca.js
@@ -16,13 +16,3 @@ const BinarySearchTree = require('./bst.js');
 const LCA = (node1, node2) => {
 
 };
-
-let bst = new BinarySearchTree();
-bst.insert(5);
-bst.insert(3);
-bst.insert(6);
-bst.insert(2);
-bst.insert(4);
-bst.insert(1);
-
-console.log(bst.root);
