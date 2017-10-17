9b99e2788f513c39a566abb01a645d64038229ec
Raymond Lee <raylee0525@gmail.com>
2017-10-17 15:04:57 -0700

    lca - bf

--- Graduated/W1/lca.js
+++ Graduated/W1/lca.js
@@ -11,8 +11,22 @@
 //   (D)
 // The LCA of D and E would be B.
 
-const BinarySearchTree = require('./bst.js');
-
+// With parent pointer
+// Time: O(h) Space: O(h) - Brute Force
 const LCA = (node1, node2) => {
+  let seen = {};
 
+  while (node1) {
+    seen[node1] = true;
+    node1 = node1.parent;
+  }
+
+  while (node2) {
+    if (seen[node2]) return seen[node2];
+    node2 = node2.parent;
+  }
+
+  return false;
 };
+
+// Time: O(h) Space: O(1) - Depth Method
