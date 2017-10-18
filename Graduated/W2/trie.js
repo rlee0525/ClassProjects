0a4a40f26ea5c6111840ea08afce13d0da879fdd
Raymond Lee <raylee0525@gmail.com>
2017-10-17 23:51:57 -0700

    trie

---
+++ Graduated/W2/trie.js
@@ -0,0 +1,22 @@
+class Trie {
+  constructor() {
+    this.root = {};
+  }
+
+  insert(word) {
+    let currentNode = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      if (!(word[i] in currentNode)) {
+        currentNode[word[i]] = {};
+      }
+
+      currentNode = currentNode[word[i]];
+    }
+  }
+}
+
+let dictionary = new Trie();
+dictionary.insert("hello");
+dictionary.insert("helie");
+console.log(dictionary.root["h"]["e"]);
